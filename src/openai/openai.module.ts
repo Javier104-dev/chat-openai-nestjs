99094b34d3f19ca4520a32e7d1b04608d04b3ad8
Javier104-dev <javieranibalvillca@gmail.com>
2024-07-03 17:14:43 -0300

    Configura openai.module

--- src/openai/openai.module.ts
+++ src/openai/openai.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { OpenaiController } from './openai.controller';
 import { OpenaiService } from './openai.service';
+import OpenAI from 'openai';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [OpenaiController],
-  providers: [OpenaiService],
+  imports: [ConfigModule],
+  providers: [
+    OpenaiService,
+    {
+      provide: OpenAI,
+      useFactory: (configureService: ConfigService) =>
+        new OpenAI({ apiKey: configureService.getOrThrow('OPENAI_API_KEY') }),
+      inject: [ConfigService],
+    },
+  ],
 })
 export class OpenaiModule {}
