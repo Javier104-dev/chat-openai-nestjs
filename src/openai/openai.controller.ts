714f7e588c2a19a0ec23722d89a182734fb0d9b3
Javier104-dev <javieranibalvillca@gmail.com>
2024-07-03 17:41:12 -0300

    Termina capa servicios

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateChatCompletionRequestDto } from './dto/create-chat-completion-request.dto';
+import { OpenaiService } from './openai.service';
 
 @Controller('api/v1/openai')
 export class OpenaiController {
+  constructor(private readonly openaiService: OpenaiService) {}
+
   @Post('chat-completion')
   async createChatCompletion(@Body() dto: CreateChatCompletionRequestDto) {
-    
+    return this.openaiService.createChatCompletion(dto.messages);
   }
 }
